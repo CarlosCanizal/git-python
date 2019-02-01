bd3f87e5dc8adc32279531bb47031206c12a6dbe
CarlosCanizal <carlos@canizal.com>
2019-02-01 08:40:46 -0600

    Automated commit

--- run_git.py
+++ run_git.py
@@ -2,4 +2,4 @@ import subprocess
 subprocess.call(["git", "add", "."])
 subprocess.call(["git","commit", "-m", "Automated commit"])
 subprocess.call(["git", "push", "origin", "master"])
-print("Hello World")
+print("Commit Done!")
